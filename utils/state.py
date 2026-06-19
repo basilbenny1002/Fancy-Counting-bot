9fda779a8502573dda8c4fe6f102e6cb38588ebd
basilbenny1002 <basilbenny1002@gmail.com>
2026-06-19 13:39:11 +0530

    Add state values to mute a user for an hour

--- utils/state.py
+++ utils/state.py
@@ -1,5 +1,6 @@
 import json
 import os
+import time
 
 STATE_FILE = "data/state.json"
 
@@ -41,4 +42,38 @@ def update_guild_state(guild_id, **kwargs):
         }
     for k, v in kwargs.items():
         state[guild_id_str][k] = v
+    save_state(state)
+
+def get_muted_user(user_id):
+    state = load_state()
+    if "muted_users" not in state:
+        state["muted_users"] = {}
+        
+    user_str = str(user_id)
+    user_data = state["muted_users"].get(user_str)
+    
+    if user_data:
+        # Check if timeout expired
+        if time.time() > user_data.get("muted_until", 0):
+            # Expired, delete the user details as requested
+            del state["muted_users"][user_str]
+            save_state(state)
+            return None
+        return user_data
+    return None
+
+def update_muted_user(user_id, muted_until=None, ping_count=None):
+    state = load_state()
+    if "muted_users" not in state:
+        state["muted_users"] = {}
+        
+    user_str = str(user_id)
+    if user_str not in state["muted_users"]:
+        state["muted_users"][user_str] = {"muted_until": 0, "ping_count": 0}
+        
+    if muted_until is not None:
+        state["muted_users"][user_str]["muted_until"] = muted_until
+    if ping_count is not None:
+        state["muted_users"][user_str]["ping_count"] = ping_count
+        
     save_state(state)
